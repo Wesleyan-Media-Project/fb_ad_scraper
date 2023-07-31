0ec59b6369ae01c4e1406c513c3c5e15c1be32ea
Pavel <poleinikov@wesleyan.edu>
2023-07-31 13:07:10 -0400

    Update insert_ad_ids_into_queue.R

--- insert_ad_ids_into_queue.R
+++ insert_ad_ids_into_queue.R
@@ -7,45 +7,20 @@ library(bigrquery)
 ## this is on wesmedia3
 conn = dbConnect(RMySQL::MySQL(), host="localhost",
                  user="xxxxx", password="xxxxx",
-                 dbname="ad_media")
+                 dbname="dbase1")
 
-## authorize the project to enable access to BigQuery
-bq_auth(path="/home/poleinikov/wmp-laura.json")
-
-q = "select page_id, ad_id, date from fbri.ad_queue where date = 'zzz';"
-q = gsub('zzz', Sys.Date(), q)
-
-d = bq_table_download(
-      bq_project_query(
-          x = 'wmp-laura',
-          query = q),
-      bigint = "integer64")
-
-d %>% mutate(ad_id = as.character(ad_id),
-             page_id = as.character(page_id)) -> d2
-
-dbWriteTable(conn, name="ad_queue", value=d2,
-             overwrite=F, append=T, row.names=F)
-
-q = "INSERT INTO ad_queue SELECT distinct page_id, id, 'zzzz' as date from textsim_new.race2022"
+## this query will insert all available ad_ids into the ad_queue table
+q = "INSERT INTO ad_queue SELECT distinct page_id, id, 'zzzz' as date from race2022"
 q = gsub('zzzz', strftime(Sys.Date(), "%Y-%m-%d"), q)
 x = dbGetQuery(conn, q)
 
-##
-## this part was added after I redirected back-pull to write into textsim_utf8.race2022_utf8
-##
-q = "INSERT INTO ad_queue SELECT distinct page_id, id, 'zzzz' as date from textsim_utf8.race2022_utf8"
-q = gsub('zzzz', strftime(Sys.Date(), "%Y-%m-%d"), q)
-x = dbGetQuery(conn, q)
-
-# dbDisconnect(conn)
-# quit('no')
-
-## the part below was used to prevent repeated downloads
-## of the same ad, but now I do not need it.
+## then, we delete the rows for ads that have already been scraped
+## their ids are present in the fb_ads_media table
 q = "drop table if exists tmp_ad_queue"
 x = dbGetQuery(conn, q)
 
+## this is the query
+## it uses a temporary table
 q = "create temporary table tmp_ad_queue as 
   select * from (
   with a as (select distinct ad_id, page_id, date from ad_queue),
@@ -66,8 +41,3 @@ x = dbGetQuery(conn, q)
 dbDisconnect(conn)
 
 quit('no')
-
-
-## in images - 479 bytes - letter "f" for Facebook. Probably says that you need to log in
-
-
